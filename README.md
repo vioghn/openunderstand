f5f542ef0c6e61980dfc600fb5d2061138fb7131
vioghn <banafshehgholinejad802@gmail.com>
2022-05-12 01:58:55 +0430

    main Project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# openunderstand
+# OpenUnderstand
+![OpenUnderstand Logo](docs/figs/OpenUnderstand_Logo.png)
+
+An open-source implementation of Understand API in Python.
+
+## Useful links
+
+* OpenUnderstand Documentation Website: [www.m-zakeri.github.io/OpenUnderstand.](https://m-zakeri.github.io/OpenUnderstand/)
---
+++ benchmark/calculator_app/src/com/calculator/app/method/printLog.java
@@ -0,0 +1,24 @@
+package com.calculator.app.method;
+
+import com.calculator.app.display.println;
+
+// this class is semantically shouldn't be here ,
+// and should be move to display package that is semantically close to this class
+// so this class is a good candidate for moving class refactoring
+
+
+public class X
+{ }
+public class printLog extends println {
+int a()
+{}
+
+ static int x(int a ) {}
+
+  @Override
+  static void print(String text) {
+        super.print(text);
+    }
+
+    public void x(){}
+}
